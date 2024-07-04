e1956746f1d3abce4f65d1d41812f2c185c9ab73
ooyagi <t_oyagi@outlook.jp>
2024-07-04 09:55:47 +0900

    やってみた 今回の用途では onChangeで発火するほどインタラクティブな動作をしない予定なので
    https://remix.run/docs/en/main/start/tutorial#submitting-forms-onchange
    から
    https://remix.run/docs/en/main/start/tutorial#managing-the-history-stack
    
    までは飛ばす

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,38 @@
-import {
-  Form,
-  Links,
-  Meta,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import appStylesHref from "./app.css?url";
+import { useEffect } from "react";
+import { json } from "@remix-run/node";
+import { Form, Links, Meta, NavLink, Outlet, Scripts, ScrollRestoration, useLoaderData, useNavigation } from "@remix-run/react";
+import { createEmptyContact, getContacts } from "./data";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+};
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
+};
 
 export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
+
   return (
     <html lang="en">
       <head>
@@ -23,6 +49,7 @@ export default function App() {
               <input
                 id="q"
                 aria-label="Search contacts"
+                defaultValue={ q || "" }
                 placeholder="Search"
                 type="search"
                 name="q"
@@ -34,17 +61,40 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className= {({ isActive, isPending }) => 
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p><i>No contacts</i></p>
+            )}
           </nav>
         </div>
-
+        <div id="detail"
+          className={ navigation.state === "loading" ? "loading" : ""} 
+        >
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
